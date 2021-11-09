463d276be36940e5e3dbc70a7a35a8af21debc14
Ashley Sands <as223343@falmouth.ac.uk>
2021-11-09 20:13:03 +0000

    Finished adding the basic layout

--- Assets/Scripts/VOAgent2.cs
+++ Assets/Scripts/VOAgent2.cs
@@ -23,8 +23,20 @@ public class VOAgent2 : MonoBehaviour
 
 	[Header( "Agent Detect Setup" )]
 	private const int maxDetectAgents = 3;  // define as const so its the same for all agents. ( i could define it in VOAgentTick, so it can be changed in the inspector)
-	private RaycastHit2D[] detectedAgents = new RaycastHit2D[ maxDetectAgents ];
+	private RaycastHit2D[] detectedAgents = new RaycastHit2D[ maxDetectAgents + 1 ];	// We must initalize with an extra element since it will detect self.
 	[SerializeField] private float detectRadius = 3f;
+	[SerializeField] private LayerMask detectAgentLayerMask;
+
+	// Current avoid Agent (if any)
+
+	private VOAgent2 currentAvoidAgent;
+	private float currentAvoidAgentDistance;
+
+	[Header( "Target Config" )]
+	[SerializeField] private Transform target;
+	private Vector3 TargetPosition => target != null ? target.position : Vector3.zero;
+
+	public bool DEBUG_DRAW = true;
 
 	private void Start()
 	{
@@ -37,7 +49,21 @@ public class VOAgent2 : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		
+
+		// Cast a circel into the scene at out current location, to find if any other NPCs are in range.
+		// Ourself will be in the result, and must be ignored.
+
+		int rayHitCount = Physics2D.CircleCastNonAlloc( transform.position, detectRadius, Vector3.zero, detectedAgents, 0, detectAgentLayerMask );
+
+		if ( rayHitCount > 1 )
+		{
+
+		}
+		else
+		{
+			// go to target things.
+		}
+
 	}
 
 	/// <summary>
@@ -54,12 +80,36 @@ public class VOAgent2 : MonoBehaviour
 
 	}
 
+	private void CalculateAvoidAgent( int rayHitCount )
+	{
+		
+	}
+
 	/// <summary>
 	/// Data that needs to take effect once all of the agents have been updated in Fixed update.
 	/// </summary>
 	[System.Serializable]
 	struct TickData
 	{
+
+	}
+
+
+	// Debug
+
+	private void OnDrawGizmos()
+	{
+		if ( !DEBUG_DRAW )
+			return;
+
+		
+
+		if ( currentAvoidAgent == null )
+			Gizmos.color = new Color( 0, 1, 0, 0.05f );
+		else
+			Gizmos.color = new Color( 1, 0, 0, 0.05f );
+
+		Gizmos.DrawSphere( transform.position, detectRadius );
 
 	}
 
