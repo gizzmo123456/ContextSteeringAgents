9e1393b8bde06e62768fa78dc6405fb9d4ecc658
Ashley Sands <as223343@falmouth.ac.uk>
2021-11-18 14:05:10 +0000

    Added flow agent PF

---
+++ Assets/Scripts/CSAgent_FlowFeild.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSAgent_FlowFeild : CSAgent
+{
+
+	[Header( "FlowFeid" )]
+	[SerializeField] private FlowFeild flowFeild;
+
+
+	[Header( "Target" )]
+	public float targetLength = 2;
+	private Vector3 direction = Vector3.zero;
+	protected override Vector3 TargetPosition => (direction * targetLength) + transform.position;
+
+	protected override void UpdateAgent()
+	{
+		Vector2 dir;
+
+		if( flowFeild.GetFlowFeildDirectionVector( transform.position, out dir ) )
+		{
+			direction = dir;
+			if ( DEBUG )
+				print("set " + dir);
+		}
+		else if ( DEBUG )
+			print( "not set" );
+
+		Debug.DrawLine( transform.position, TargetPosition, Color.blue, Time.deltaTime );
+
+	}
+
+}
